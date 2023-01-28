3a1576c8d664d789c839021b6e34a00385a286b6
David Carlisle <d.p.carlisle@gmail.com>
2023-01-28 10:55:09 +0000

    l3build  build.lua

---
+++ build.lua
@@ -0,0 +1,10 @@
+module = "xxetex"
+
+sourcefiles={"xxetex.tex","xxetex.lua","xxelatex.ini"}
+installfiles={"xxetex.tex","xxetex.lua","xxelatex.ini"}
+
+tdslocations={
+"tex/xxetex/base/xxetex.tex",
+"tex/xxetex/base/xxetex.lua",
+"tex/xxetex/base/xxelatex.ini"
+}
